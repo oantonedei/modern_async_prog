ecac65c70c7c0baba344c0a46881c9425a3ec59d
Antonedei Wonimidei Otoro <otoroj@engineer.com>
2022-11-15 21:52:46 -0600

    added files for Lab 9

---
+++ Lab9/index.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+  refreshEmp();
+};
+
+async function refreshEmp() {
+  document.getElementById("display").innerHTML = "";
+  const response = await fetch("https://randomuser.me/api/?results=5");
+  const employee = await response.json();
+
+  let html = `<h3 style="text-align: center">Employee List</h3>`;
+  employee.results.forEach((emp) => {
+    html += `
+        <div class="display-container">
+          <img class="img" src="${emp.picture.large}" alt="dog" />
+          <div class="container">
+            <h2>${emp.name.first} ${emp.name.last}</h2>
+            <p>
+              Phone: ${emp.phone}<br />
+              ${emp.email}
+            </p>
+          </div>
+        </div>
+        `;
+  });
+  html += `<button class="btn" onclick="refreshEmp();">
+      Refresh
+    </button>`;
+  document.getElementById("display").innerHTML += html;
+}
